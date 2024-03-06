bd459335cba661bebd7e49ffb255e1ceafe129a7
jacoboval <jacoboval@gmail.com>
2024-03-06 00:52:05 -0600

    Se actualiza secion 9

--- Seccion 9 - Funciones Python/Funciones.py
+++ Seccion 9 - Funciones Python/Funciones.py
@@ -34,14 +34,38 @@
 
 ## 75  Argumenos variables en funciones
 
-def listarNombres(*nombres):  # se convierte en tupla  *args
-    for nombre in nombres:
-        print(nombre)
-
-listarNombres('jacobo', 'alma', 'karla', 'leslie', 'ivon' )
-listarNombres('laura','daniel')
-
-
+# def listarNombres(*nombres):  # se convierte en tupla  *args
+#     for nombre in nombres:
+#         print(nombre)
+#
+# listarNombres('jacobo', 'alma', 'karla', 'leslie', 'ivon' )
+# listarNombres('laura','daniel')
 
 
+##  76  EJERCICIO ##
+
+def sumar_var(*args):
+    resultado = 0
+    for valor in args:
+        resultado += valor
+    return resultado
+
+print(sumar_var(3,5,6))
+
+##  78  EJERCICIO ##
+def multiplicar_valore(*numeros):
+    resultado = 1
+    for numero in numeros:
+        resultado  *= numero
+    return resultado
+print(multiplicar_valore(3,4,6,12))
+
+## argumentos variables llave - valor
+
+def listarTerminos(**terminos):
+    for llave, valor in terminos.items():
+        print(f'{llave}: {valor}')
+
+listarTerminos(IDE='Integrated Development Environmet')
+listarTerminos(DBMS='Database Management System')
 
